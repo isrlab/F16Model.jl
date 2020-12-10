c319401ae99de6cad25f556bbde07998f6546ae6
Intelligent Systems Research Laboratory <raktim@tamu.edu>
2020-12-09 23:44:48 -0600

    Moved

--- src/F16Model.jl
+++ src/F16Model.jl
@@ -1,7 +1,7 @@
 module F16Model
 using FileIO, GridInterpolations, ForwardDiff
 
-global F16AeroData = load("data/F16AeroData.jld2"); # Loads the Aero Tables for F16.
+global F16AeroData = load("F16AeroData.jld2"); # Loads the Aero Tables for F16.
 
 
 #include all the files
