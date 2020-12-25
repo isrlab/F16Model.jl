1d1e17c3e73a2ba22d6051487613ecdfa3e3ba74
Intelligent Systems Research Laboratory <raktim@tamu.edu>
2020-12-24 20:33:03 -0600

    updated path of aerotable

--- src/AeroTableInterpolation.jl
+++ src/AeroTableInterpolation.jl
@@ -1,6 +1,6 @@
 using HDF5, Interpolations
 
-fid = h5open("src/F16AeroData.h5", "r");
+fid = h5open("F16AeroData.h5", "r");
 
 function createAeroFunction(data,indepVars...)
     len = length.(indepVars);
